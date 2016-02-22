e153f1fa51366bebe51fa14fa39ddbcf9adc8222
spolydor-tgm <spolydor-tgm@users.noreply.github.com>
2016-02-22 17:34:31 +0100

    Added files via upload

--- CsvTest.py
+++ CsvTest.py
@@ -1,6 +1,6 @@
 import unittest
 
-from Csv import Csv
+from src.CSV.Csv import Csv
 
 
 class CSVtest(unittest.TestCase):
@@ -9,16 +9,42 @@ class CSVtest(unittest.TestCase):
         self.c = Csv('csv_file.csv')
 
     def test_load(self):
-        try:
-            self.cc = Csv('tc.csv')
-        except FileNotFoundError as e:
-            self.fail("Where is the CSV-file?\n %s" % e)
+         self.cc = Csv('tc.csv')
+         self.cc.read()
 
-    def test_load(self):
+    def test_load2(self):
         try:
-            self.c.read()
+            self.assertRaises(FileNotFoundError, self.c.read())
         except FileNotFoundError as e:
-            self.fail("Where is the CSV-file?\n %s" % e)
+            self.fail("CSV-file?\n %s" % e)
+
+    def test_load3(self):
+        try:
+            self.cc = Csv('tc2.csv')
+            t = self.cc.read()
+            txt = "1, abc\n18, ae\n19, ai\n"
+            self.assertTrue(t == txt, "good job")
+        except IOError as e:
+            self.fail("CSV-file?\n %s" % e)
+
+    def test_load4(self):
+        try:
+            self.cc = Csv('tc2.csv')
+            t = self.cc.read()
+            txt = "1, abc\n\n18, ae\n19, ai\n"
+            self.assertFalse(t == txt, "Nice try")
+        except IOError as e:
+            self.fail("multipe whitespaces\n %s" % e)
+
+    def test_load5(self):
+        try:
+            self.cc = Csv('tc2.csv')
+            t = self.cc.read()
+            txt = "1, abc\n18, ae\n19, ai\na"
+            self.assertFalse(t == txt, "Nice try")
+        except IOError as e:
+            self.fail("too much characters\n %s" % e)
+
 
 
 if __name__ == '__main__':
