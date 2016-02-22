17df8244a0c535875e56f073c315f066313a8007
spolydor-tgm <spolydor-tgm@users.noreply.github.com>
2016-02-22 14:00:00 +0100

    Added files via upload

---
+++ CsvTest.py
@@ -0,0 +1,19 @@
+import unittest
+
+from src.CSV.Csv import Csv
+
+
+class CSVtest(unittest.TestCase):
+#test
+    def setUp(self):
+        self.c = Csv('csv_file.csv')
+
+    def test_load(self):
+        try:
+            self.c.read()
+        except FileNotFoundError as e:
+            self.fail("Where is the CSV-file?\n %s" % e)
+
+
+if __name__ == '__main__':
+    unittest.main()
